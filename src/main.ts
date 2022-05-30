e52649600cf36c8841c8738872243c15ad3eeee5
Zin Min Htet <zetminhtin@gmail.com>
2022-05-31 00:56:26 +0630

    added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 const PORT = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   await app.listen(PORT);
 }
 bootstrap();
