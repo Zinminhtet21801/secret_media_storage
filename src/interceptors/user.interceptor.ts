557f9f73dbe1024d8510512b6ac42a517f6e445b
Zin Min Htet <zetminhtin@gmail.com>
2022-05-31 00:57:17 +0630

    user interceptor to exclude user password

---
+++ src/interceptors/user.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  CallHandler,
+  ClassSerializerInterceptor,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+interface ClassConstructor {
+  new (...args: any[]);
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // Run something before a request is handled by the request handler
+    // console.log('Before...', context);
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
