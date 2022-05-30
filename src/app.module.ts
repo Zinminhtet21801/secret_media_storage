1c2aa94264cc3801473cc812edb4017b5f6b22d9
Zin Min Htet <zetminhtin@gmail.com>
2022-05-31 00:56:16 +0630

    added global validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Module,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(new DataSource(require('./ormconfig.ts'))),
-    UserModule,
+  imports: [TypeOrmModule.forRoot(require('./ormconfig')), UserModule],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
   ],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
 })
 export class AppModule {}
