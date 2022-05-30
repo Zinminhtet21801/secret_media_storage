18d529a1463e0c9306271510e07db1095c58cf8b
Zin Min Htet <zetminhtin@gmail.com>
2022-05-31 00:58:14 +0630

    added login create delete user and serialize user data to exclude password to return

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserCreateDTO } from './dtos/user-create.dto';
+import { UserDTO } from './dtos/user.dto';
+import { Serialize } from '../interceptors/user.interceptor';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+@Serialize(UserDTO)
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Post('create')
+  async saveUser(@Body() body: UserCreateDTO) {
+    const user = await this.userService.saveUser(body);
+    return user;
+  }
+
+  @Post('login')
+  async login(@Body() body: Partial<UserCreateDTO>) {
+    console.log(body);
+
+    const user = await this.userService.login(body.email, body.password);
+    return user;
+  }
+
+  @Post('delete')
+  async deleteUser(@Body() body: Partial<UserCreateDTO>) {
+    const user = await this.userService.deleteUser(body.email);
+    return user;
+  }
+}
