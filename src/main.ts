ac5d29483a13c4d8fd22092b8bca5a5625a21c5e
Zin Min Htet <zetminhtin@gmail.com>
2022-06-01 01:44:26 +0630

    configured main.ts for hbs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import helmet from 'helmet';
+import { join } from 'path';
 import { AppModule } from './app.module';
 const PORT = process.env.PORT || 5000;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(helmet());
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   await app.listen(PORT);
 }
 bootstrap();
