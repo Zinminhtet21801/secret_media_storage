cc24694ce3405df7ef6d5d65773970cb0646b2f3
Zin Min Htet <zetminhtin@gmail.com>
2022-06-01 01:45:53 +0630

    added IsEmail and Min validator to user-create.dto

--- src/user/dtos/user-create.dto.ts
+++ src/user/dtos/user-create.dto.ts
@@ -1,12 +1,15 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, Min } from 'class-validator';
 
 export class UserCreateDTO {
   @IsNotEmpty()
   name: string;
 
   @IsNotEmpty()
+  @IsEmail()
+  @Min(1)
   email: string;
 
   @IsNotEmpty()
+  @Min(5)
   password: string;
 }
