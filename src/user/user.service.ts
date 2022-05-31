3c440309f4d5f11bfde80e0bbb430228820b96ca
Zin Min Htet <zetminhtin@gmail.com>
2022-06-01 01:45:23 +0630

    added forgotPassword and resetPassword services

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 import { UserCreateDTO } from './dtos/user-create.dto';
 import { User } from './user.entity';
 import * as bcrypt from 'bcrypt';
-
+const nodemailer = require('nodemailer');
 @Injectable()
 export class UserService {
   constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
@@ -52,5 +52,63 @@ export class UserService {
     }
     const deletedUser = await this.userRepo.delete(user.id);
     return deletedUser;
+  }
+
+  async forgotPassword(userEmail: string) {
+    const { id, name, email } = await this.findUser(userEmail);
+    if (!email) {
+      throw new BadRequestException('User not found!!!');
+    }
+
+    // host: 'smtp.ethereal.email',
+    // port: 587,
+    // secure: false,
+    // auth: {
+    //   user: process.env.ETHEREAL_USERNAME,
+    //   pass: process.env.ETHEREAL_PASSWORD,
+    // },
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD,
+      },
+    });
+
+    const mailDetails = {
+      from: process.env.GMAIL_USERNAME,
+      to: email,
+      subject: 'Forgot Password',
+      text: `Hi ${name},
+    You have requested to reset your password.
+    Please click on the link below to reset your password.
+    ${process.env.BASE_URL}/user/reset-password/${email}
+    `,
+    };
+
+    const info = await transporter.sendMail(mailDetails, function (err, data) {
+      if (err) {
+        console.log('Error Occurs', err);
+      } else {
+        console.log('Email sent successfully');
+      }
+    });
+    // console.log('Message sent: %s', info.messageId);
+    // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  }
+
+  async resetPassword(inputEmail: string, inputPassword: string) {
+    console.log(inputEmail, inputPassword);
+    
+    const user = await this.findUser(inputEmail);
+    if (!user) {
+      throw new BadRequestException('User not found!!!');
+    }
+    const hashedPassword = await this.hashPassword(inputPassword);
+    const updatedUser = await this.userRepo.update(user.id, {
+      password: hashedPassword,
+    });
+    return updatedUser;
   }
 }
