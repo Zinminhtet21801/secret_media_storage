852519a8553c2e4d294889f4e849279c71190f75
Zin Min Htet <zetminhtin@gmail.com>
2022-06-01 01:44:10 +0630

    env ConfigModule.forRoot

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,8 +8,13 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [TypeOrmModule.forRoot(require('./ormconfig')), UserModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` }),
+    TypeOrmModule.forRoot(require('./ormconfig')),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
