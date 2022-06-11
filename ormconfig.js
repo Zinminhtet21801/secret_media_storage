d83d8da0e31064fa10819fe461b2ef6f2238d931
Zin Min Htet <zetminhtin@gmail.com>
2022-06-11 23:23:35 +0630

    added typeorm config

---
+++ ormconfig.js
@@ -0,0 +1,59 @@
+var dotenv = require('dotenv');
+var fs = require('fs');
+var path = require('path');
+// var variable = fs.readFileSync(path.resolve(__dirname, '.env.production'),'utf8');
+// console.log(__dirname,dotenv.config({ path: '.env.production' }), variable);
+var parsed = dotenv.config({ path: '.env.production' });
+var dbConfig = {
+  synchronize: false,
+  migrations: ['migrations/*.js'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+      synchronize: true,
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+      migrationsRun: true,
+    });
+    break;
+  case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.HEROKU_POSTGRES_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    break;
+  default:
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: parsed.parsed.HEROKU_POSTGRES_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    break;
+  // throw new Error('unknown environment');
+}
+
+console.log(process.env.NODE_ENV, dbConfig);
+
+module.exports = dbConfig;
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,8 +1,19 @@
-const config = {
-  type: 'sqlite',
-  database: 'db.sqlite',
-  entities: ['**/*.entity.js'],
-  synchronize: true,
-};
+const dbConfig = require('../ormconfig');
+import { DataSource } from "typeorm"
+// const source = new DataSource({
+//   type: 'sqlite',
+//   synchronize: process.env.NODE_ENV === 'test' ? true : false,
+//   migrations: ['migrations/*.js'],
+//   entities:
+//     process.env.NODE_ENV === 'test' ? ['**/*.entity.ts'] : ['**/*.entity.js'],
+//   database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
+//   migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
+// });
 
-module.exports = config;
+// console.log(dbConfig);
+
+const ormconfig = new DataSource({
+  ...dbConfig,
+});
+
+export default ormconfig;
