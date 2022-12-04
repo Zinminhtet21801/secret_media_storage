d8a51d6fc508e472b4659fcc6a8dd285aac35c3e
ZinMinHtet21801 <zetminhtin@gmail.com>
2022-12-04 21:05:12 +0630

    added getItemById, getItemByName downloadMedia and deleteMediaItem services

--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { unlink } from 'fs';
 import { AuthService } from 'src/auth/auth.service';
-import { Repository } from 'typeorm';
+import { In, Not, Repository } from 'typeorm';
+import { fileTypeMatcherHelpers } from './helpers/filepath.helpers';
 import { Media } from './media.entity';
 
 @Injectable()
@@ -11,18 +13,48 @@ export class MediaService {
     private readonly authService: AuthService,
   ) {}
 
+  async getItemById(id: string) {
+    try {
+      const res = await this.mediaRepo.findOne({
+        where: {
+          id,
+        },
+      });
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getItemByName(name: string) {
+    try {
+      const res = await this.mediaRepo.findOne({
+        where: {
+          name,
+        },
+      });
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async uploadFiles(
     user: object,
     userEmail: string,
     referenceFileName: string,
     mimeType: string,
   ) {
+    const type = fileTypeMatcherHelpers({ mimeType });
+    const checkFileExist = await this.getItemByName(referenceFileName);
+    if (checkFileExist) {
+      throw new HttpException('File already exist', 400);
+    }
     const newFile = await this.mediaRepo.create({
-      name: `${userEmail}/${referenceFileName}`,
-      user: user,
-      type: mimeType,
+      name: `${referenceFileName}`,
+      user,
+      type,
     });
-    console.log(newFile, 'newFile', user);
 
     const res = await this.mediaRepo.save(newFile);
     return res;
@@ -79,16 +111,79 @@ export class MediaService {
           user: {
             id: userId.toString(),
           },
-          type: category,
+          type:
+            category === 'others'
+              ? Not(In(['image', 'audio', 'video']))
+              : category,
         },
+
         skip: (Number(page) - 1) * 10,
         take: 10,
       });
+
       return {
         data: res,
       };
     } catch (error) {
       console.log(error);
     }
+  }
+
+  /**
+   * It deletes a media item from the database
+   * @param {string} userId - The id of the user who is deleting the media item.
+   * @param {string} mediaId - The id of the media item you want to delete.
+   * @returns The number of rows affected by the delete operation.
+   */
+  async deleteMediaItem(userId: string, email: string, mediaId: string) {
+    try {
+      const file = await this.getItemById(mediaId);
+      console.log('====================================');
+      console.log(file);
+      console.log('====================================');
+      if (!file) {
+        return {
+          message: 'File not found',
+        };
+      } else {
+        const res = await this.mediaRepo.delete({
+          id: mediaId,
+        });
+        unlink(`./uploads/${email}/${file.type}/${file.name}`, (error) => {
+          if (error) {
+            console.log(error);
+          }
+        });
+        // console.log(res);
+        return res;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async downloadMedia(
+    userId: number,
+    id: string,
+    email: string,
+    category: string,
+  ) {
+    const res = await this.mediaRepo.findOne({
+      where: {
+        id,
+        user: {
+          id: userId.toString(),
+        },
+      },
+    });
+    // const media = res.name.split(`${email}/`)[1];
+    // return readFileSync(`./uploads/${email}/${category}/${media}`);
+    // // return res;
+    // const media = res.name.split(`${email}/`)[1];
+    // return createReadStream(join(`./uploads/${email}/${category}/${media}`), {
+    //   autoClose: true,
+    // });
+
+    return res;
   }
 }
