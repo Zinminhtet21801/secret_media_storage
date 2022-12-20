f8e1dc880aa70be20a45ea2201c43710eaa66916
ZinMinHtet21801 <zetminhtin@gmail.com>
2022-12-20 21:53:29 +0630

    swagger initialized

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,22 @@ import helmet from 'helmet';
 import { join } from 'path';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const express = require('express');
 const PORT = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use('/uploads', express.static('uploads'));
+  const config = new DocumentBuilder()
+    .setTitle('Secret Media Storage API')
+    .setDescription('API for Secret Media Storage')
+    .setVersion('1.0')
+    .addTag('secret-media-storage')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.enableCors({
     origin: process.env.FRONT_END_URL,
     credentials: true,
