7f55cb024926915012e7b455b0b765d2980891db
ZinMinHtet21801 <zetminhtin@gmail.com>
2022-12-26 16:08:12 +0630

    checking jwt in cookie instead of authorization header

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -11,22 +11,26 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     // Add your custom authentication logic here
     // for example, call super.logIn(request) to establish a session.
     const req = context.switchToHttp().getRequest();
+    // console.log('====================================');
+    // console.log(req?.headers?.cookie);
+    // console.log('====================================');
+    // if (!req?.headers?.authorization?.split(' ')[1]) {
+    //   throw new UnauthorizedException();
+    // }
 
-    if (!req?.headers?.authorization?.split(' ')[1]) {
+    if (!req?.headers?.cookie?.split('=')[1]) {
       throw new UnauthorizedException();
     }
 
     return super.canActivate(context);
-
-    // return req.cookies.token ? true : false;
   }
 
-  handleRequest(err, user, info: Error, context: any, status: any) {
-    if (err || info || !user) {
-      // throw err || info || new UnauthorizedException();
-      throw new UnauthorizedException();
-    }
+  // handleRequest(err, user, info: Error, context: any, status: any) {
+  //   if (err || info || !user) {
+  //     // throw err || info || new UnauthorizedException();
+  //     throw new UnauthorizedException();
+  //   }
 
-    return user;
-  }
+  //   return user;
+  // }
 }
