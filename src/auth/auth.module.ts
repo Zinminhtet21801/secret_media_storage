0dc2ce0795b87411c66ae6711bee99cc791b486e
ZinMinHtet21801 <zetminhtin@gmail.com>
2023-01-03 22:15:33 +0630

    idk

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,19 +12,19 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
 let parsedEnv;
 
 if (process.env.NODE_ENV === 'production') {
-  // parsedEnv = {
-  //   ETHEREAL_NAME: process.env.ETHEREAL_NAME,
-  //   ETHEREAL_USERNAME: process.env.ETHEREAL_USERNAME,
-  //   ETHEREAL_PASSWORD: process.env.ETHEREAL_PASSWORD,
-  //   BASE_URL: process.env.BASE_URL,
-  //   GMAIL_USERNAME: process.env.GMAIL_USERNAME,
-  //   GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
-  //   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-  //   JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY,
-  //   JWT_EXPIRATION_TIME: process.env.JWT_EXPIRATION_TIME,
-  //   FRONT_END_URL: process.env.FRONT_END_URL,
-  // };
-  parsedEnv = dotenv.config({ path: `.env.production` }).parsed;
+  parsedEnv = {
+    ETHEREAL_NAME: process.env.ETHEREAL_NAME,
+    ETHEREAL_USERNAME: process.env.ETHEREAL_USERNAME,
+    ETHEREAL_PASSWORD: process.env.ETHEREAL_PASSWORD,
+    BASE_URL: process.env.BASE_URL,
+    GMAIL_USERNAME: process.env.GMAIL_USERNAME,
+    GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY,
+    JWT_EXPIRATION_TIME: process.env.JWT_EXPIRATION_TIME,
+    FRONT_END_URL: process.env.FRONT_END_URL,
+  };
+  // parsedEnv = dotenv.config({ path: `.env` }).parsed;
 } else {
   parsedEnv = dotenv.config({ path: `.env.development` }).parsed;
 }
